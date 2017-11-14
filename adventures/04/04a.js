0cdc9444527e71a408be682b19aec512dc193a36
Alex @ mbp <AlexWang-16@users.noreply.github.com>
2017-11-14 12:53:43 -0500

    updated 04a adventure on arrow function behavior with 'this' keyword when being used as method of an object

--- adventures/04/04a.js
+++ adventures/04/04a.js
@@ -15,3 +15,35 @@ let g = function() {
 
 console.log(g() === global) // True
 
+//  Example 2
+//  What happens if an arrow function is used as a method inside an object?
+
+let cat = {
+  species: "cat",
+  talk: () => `${this.name}: meow!`,
+  speak: function() { return `${this.name}: meow!` }
+}
+
+let munchkin = {
+  name: "fluffly"
+}
+
+Object.setPrototypeOf(munchkin, cat) // Sets cat to be prototype of munchkin
+
+console.log(munchkin.name)
+console.log(munchkin.__proto__)
+console.log(munchkin.talk())  // 'undefined: meow!'
+
+this.name = 'ahahaha'
+console.log(munchkin.talk())  // 'ahahaha: meow!'
+console.log(munchkin.speak()) // 'fluffy: meow!'
+
+//  We see that because arrow function does not have the concept of "this"
+//  "this" will mean the surrounding "thing" at the time when talk() method
+//  was invoked.
+//
+//  This means despite munchkin was the object for which talk() was invoked on,
+//  "this" was captured as the global object
+//
+//  In contrast, a regular function(){} will have the concept of this and
+//  thus this.name can be inferred
