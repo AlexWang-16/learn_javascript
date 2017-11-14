11e530787f5dc639821b70d394534ecab893924d
Alex @ mbp <AlexWang-16@users.noreply.github.com>
2017-11-14 14:57:01 -0500

    updating adventure 10 on class inheritence using 'extends' keyword in ES6

--- adventures/10/10.js
+++ adventures/10/10.js
@@ -1,12 +1,13 @@
 //  Classes in JavaScript
-class Mammal {
-  constructor(sound){
-    this.sound = sound
-  }
 
-  talk(){
-    return this.sound
-  }
+class Mammal {
+ constructor(sound){
+   this.sound = sound
+ }
+
+ talk(){
+   return this.sound
+ }
 }
 
 class Dog extends Mammal {
@@ -15,6 +16,26 @@ class Dog extends Mammal {
   }
 }
 
-let fluffykins = new Dog('woof!')
-let x = fluffykins.talk()
-x
+const flufflykins = new Mammal('wooof!')
+console.log(flufflykins.talk())
+
+const snoopy = new Dog()
+console.log(snoopy.talk())
+
+//  Keep in mind that properties and methods in ES6 classes are all public.
+//  There is no way to make them private. Hence @MPJ recommends using
+//  composition rather than inheritence
+
+//  Some coders use '_' in front of propert names as a convention to indicate
+//  that the property is supposed to be private, but there is no mechanism
+//  to prevent other developers from directly accessing those properties.
+//  The reason that this behavior is not implemented is because JavaScript
+//  does not have classes! The concept of class is purely syntatic sugar.
+
+function cat (name, species){
+  this.name = name
+  this.species = species
+}
+
+let fluff = Object.create(cat)
+console.log(cat.isPrototypeOf(fluff))
